37ad4a047fd2cb05d68d1c9b861cd67f42617eb6
EmersonTeshima <emerson.sp215@gmail.com>
2024-10-21 13:32:53 -0300

    Segundo commit

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -91,7 +91,23 @@ DATABASES = {
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+ """
 
+import os
+import dj_database_url
+
+# Ambiente de desenvolvimento local
+if os.environ.get('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
